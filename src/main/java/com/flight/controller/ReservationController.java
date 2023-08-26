dc63592f72c08d7eb5e0cc75fc5f534310827746
blog <astikbandekar@gmail.com>
2023-08-26 22:09:11 +0530

    made changes in reservation controller

--- src/main/java/com/flight/controller/ReservationController.java
+++ src/main/java/com/flight/controller/ReservationController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.flight.entities.Booking;
 import com.flight.entities.Reservation;
 import com.flight.service.ReservationService;
 
